1bc899e82b117677787f6c9e8569131730e6d5f1
thesayyn <thesayyn@gmail.com>
2018-07-18 22:15:54 +0300

    chore: Update example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CardStateEvent, OffsetStateEvent } from 'ngx-swing';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  cardStateChanged(event: CardStateEvent) {
+    console.log('card state changed!', event)
+  }
+
+  offsetStateChanged(event: OffsetStateEvent) {
+    console.log('offset state changed!', event)
+  }
 }
