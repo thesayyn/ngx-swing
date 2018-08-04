96c490f0c9893a85015ed86b45b35b3266391ebf
thesayyn <thesayyn@gmail.com>
2018-08-04 12:55:07 +0300

    test: add card state tests

--- projects/ngx-swing/src/lib/swing-card.component.spec.ts
+++ projects/ngx-swing/src/lib/swing-card.component.spec.ts
@@ -1,8 +1,43 @@
-import { SwingCardComponent } from './swing-card.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { SwingCardComponent, CardState } from './swing-card.component';
+import { NgxSwingModule } from './ngx-swing.module';
+import { Offset } from './offset';
 
 describe('SwingCardComponent', () => {
-  it('should create an instance', () => {
-    // const directive = new SwingCardDirective();
-    // expect(directive).toBeTruthy();
+  let component: SwingCardComponent;
+  let fixture: ComponentFixture<SwingCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ 
+        NoopAnimationsModule,
+        NgxSwingModule
+      ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SwingCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should be CardState.free', () => {
+    expect(component.cardState).toBe(CardState.free);
+  });
+
+  it('should throw out Card', () => {
+    const offset: Offset = { x: 1, y: -1 };
+    const spy = spyOn(component, 'throwOut');
+    component.throwOut(offset);
+    expect(spy).toHaveBeenCalled();
   });
 });
