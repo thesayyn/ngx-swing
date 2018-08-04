2c5e2e844825bf70136f943e6d1924f90812d4dc
thesayyn <thesayyn@gmail.com>
2018-08-04 13:05:21 +0300

    refactor: move rotation calculation function

--- projects/ngx-swing/src/lib/utils.ts
+++ projects/ngx-swing/src/lib/utils.ts
@@ -1,12 +1,8 @@
-export function mapValueInRange(
-  value: number,
-  fromLow: number,
-  fromHigh: number,
-  toLow: number,
-  toHigh: number,
-): number {
-  const fromRangeSize = fromHigh - fromLow;
-  const toRangeSize = toHigh - toLow;
-  const valueScale = (value - fromLow) / fromRangeSize;
-  return toLow + valueScale * toRangeSize;
+import { Offset } from "./offset";
+import { Dimension } from './dimension'
+
+export function calculateRotation(dimension: Dimension, offset: Offset): number {
+  const horizontalOffset = Math.min(Math.max(offset.x / dimension.width, -1), 1);
+  const verticalOffset = (offset.y > 0 ? 1 : -1) * Math.min(Math.abs(offset.y) / 100, 1);
+  return horizontalOffset * verticalOffset
 }
